526381f805d8f760f4e0ca29381443b0e80f99ac
Gakun <jingwa@umich.edu>
2017-09-03 22:01:56 +0800

    finish algorithm 2 week2

--- algorithm_thinking/cluster_algorithm/alg_app3_solution.py
+++ algorithm_thinking/cluster_algorithm/alg_app3_solution.py
@@ -7,7 +7,7 @@ import random
 import time
 import matplotlib.pyplot as plt
 import urllib2
-import alg_project
+import copy
 
 def gen_random_clusters(num_clusters):
     """
@@ -67,9 +67,78 @@ def q1_solution():
 #q1_solution()
 
 
+DIRECTORY = "http://commondatastorage.googleapis.com/codeskulptor-assets/"
+DATA_3108_URL = DIRECTORY + "data_clustering/unifiedCancerData_3108.csv"
+DATA_896_URL = DIRECTORY + "data_clustering/unifiedCancerData_896.csv"
+DATA_290_URL = DIRECTORY + "data_clustering/unifiedCancerData_290.csv"
+DATA_111_URL = DIRECTORY + "data_clustering/unifiedCancerData_111.csv"
+
+
+def load_data_table(data_url):
+    """
+    Import a table of county-based cancer risk data
+    from a csv format file
+    """
+    data_file = urllib2.urlopen(data_url)
+    data = data_file.read()
+    data_lines = data.split('\n')
+    print "Loaded", len(data_lines), "data points"
+    data_tokens = [line.split(',') for line in data_lines]
+    return [[tokens[0], float(tokens[1]), float(tokens[2]), int(tokens[3]), float(tokens[4])]
+            for tokens in data_tokens]
+
+
 def q7_solution():
     """
     Code for Question 7
+    Hierarchical: 1.752*10^11
+    K-means: 2.713*10^11
     """
+    data_table = load_data_table(DATA_111_URL)
+    singleton_list = []
+    for line in data_table:
+        singleton_list.append(alg_cluster.Cluster(set([line[0]]), line[1], line[2], line[3], line[4]))
+
+    #cluster_list = sequential_clustering(singleton_list, 15)
+    #print "Displaying", len(cluster_list), "sequential clusters"
+
+    cluster_list = solution.hierarchical_clustering(singleton_list, 9)
+    print "Displaying", len(cluster_list), "hierarchical clusters"
+    print compute_distortion(cluster_list, data_table)
+
+    # cluster_list = solution.kmeans_clustering(singleton_list, 9, 5)
+    # print "Displaying", len(cluster_list), "k-means clusters"
+    # print compute_distortion(cluster_list, data_table)
+
+#q7_solution()
 
 
+def q10_solution():
+    """
+    Question 10
+    """
+    cluster_range = range(6, 21)
+    hier_dist = list()
+    kmeans_dist = list()
+    data_table = load_data_table(DATA_896_URL)
+    singleton_list = []
+    for line in data_table:
+        singleton_list.append(alg_cluster.Cluster(set([line[0]]), line[1], line[2], line[3], line[4]))
+    hier_singleton = copy.deepcopy(singleton_list)
+    # Compute distortion of hierarchical cluster
+    hier_cluster = solution.hierarchical_clustering(hier_singleton, 20)
+    hier_dist.append(compute_distortion(hier_cluster, data_table))
+    for num in xrange(19, 5, -1):
+        hier_cluster = solution.hierarchical_clustering(hier_cluster, num)
+        hier_dist.append(compute_distortion(hier_cluster, data_table))
+    hier_dist.reverse()
+    # Compute distortion of k-means cluster
+    for num in cluster_range:
+        kmeans_singleton = copy.deepcopy(singleton_list)
+        kmeans_dist.append(compute_distortion(solution.kmeans_clustering(kmeans_singleton, num, 5), data_table))
+    # Plot
+    compare_line_plot([cluster_range, cluster_range], [hier_dist, kmeans_dist],
+        'Number of Clusters', 'Distortion (10^12)', legend_list=['Hierarchical', 'K-means'],
+        title='Distortion of Two Clustering Methods \n- 896 Data Set')
+
+q10_solution()
